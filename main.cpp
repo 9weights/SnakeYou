632a5fad3a7c91ed03aaa414a00644d02681568b
9weights <68071656+9weights@users.noreply.github.com>
2023-12-04 00:17:53 +0000

    someting worng in moveSnake

--- main.cpp
+++ main.cpp
@@ -25,9 +25,17 @@ enum class State
 class Snake
 {
 private:
-    VecInt m_headPos{};
     VecInt m_heading{};
+    bool m_isAlive{true};
+    std::vector<int> m_position{}; //small enough to keep on stack for now
+    const VecInt& m_gridSize{};
 public:
+    Snake(const VecInt& pos, const VecInt& gridSize)
+        : m_gridSize{gridSize}
+        , m_position{}
+    {
+        m_position.emplace_back(pos.x + pos.y * m_gridSize.x);
+    }
     bool m_canMove{true};
     void askInput()
     {
@@ -37,14 +45,36 @@ public:
         if (IsKeyDown(KEY_RIGHT)){m_heading = {1,0};}
     }
 
-    void moveSnake()
+    void moveSnake(std::vector<State>& boardState)
     {
-        m_headPos.x += m_heading.x;
-        m_headPos.y += m_heading.y;
-    }
-    
-    VecInt getHeadPos() const {return m_heading;}
+        // update snake head
+        int newPosition = m_position[m_position.size() - 1] + m_heading.x + m_heading.y * m_gridSize.x;
+        bool hasEaten{false};
+        // Check if other entities occupy space and handle
+        switch (boardState[newPosition])
+        {
+        case State::wall: 
+        case State::snake:
+            m_isAlive = false;
+            break;
+        case State::empty:
+            boardState[m_position[0]] = State::empty;
+            for (auto idx{0}; idx < m_position.size() - 2; ++idx) // off by one?
+            {
+                m_position[idx] = m_position[idx + 1];
+            }
+            m_position[m_position.size() - 1] = newPosition;
+            break;
+        case State::food:
+            m_position.emplace_back(newPosition);
+            break;
+        case State::poison: // later make poison cut snake in half and turn into walls
+            break;
+        }
+        // update board
+        boardState[m_position[m_position.size() - 1]] = State::snake;
 
+    }
 };
 
 
@@ -99,18 +129,20 @@ int main()
 
     // make board vars
     const int boardSizex{20};
-    const VecInt screenDimensions{1000,800};
-    const int cellSize{screenDimensions.x / boardSizex};
-    const VecInt boardSize{boardSizex,screenDimensions.y / cellSize};
+    const VecInt targetScreenDimensions{1000,800};
+    const int cellSize{targetScreenDimensions.x / boardSizex};
+    const VecInt boardSize{boardSizex,targetScreenDimensions.y / cellSize};
+    const VecInt screenDimensions{targetScreenDimensions.x, boardSize.y * cellSize};
     const int nBoardCells{boardSize.x * boardSize.y};
     std::vector<State> boardState(nBoardCells);
     // initialise other vars
-    Snake snake{};
+    Snake snake{VecInt {boardSize.x/2,boardSize.y/2}, boardSize};
     Camera2D camera{};
     camera.zoom = 1;
     camera.offset = {0,0};
     camera.target = {0,0};
     camera.rotation = {0};
+    InitWindow(screenDimensions.x, screenDimensions.y, "Snake GUI");
 
     // welcome screen
     bool isMenu{false};
@@ -122,12 +154,12 @@ int main()
     // gameplay loop
     bool isPlay {true};
 
-    while (isPlay)
+    while (!WindowShouldClose())
     {
         if (snake.m_canMove)
         {
             snake.askInput();
-            snake.moveSnake();
+            snake.moveSnake(boardState);
         }
 
         renderScene(boardState, boardSize, cellSize, camera);
